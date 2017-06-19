1e8c86f2a62f28d4c36ebffba5e9642e6cdfc823
Sam <slin@masterlink.com.tw>
2017-06-19 17:43:28 +0800

    make sample for git demo

---
+++ test.cs
@@ -0,0 +1,38 @@
+
+
+class People
+{
+	public string Name()
+	{
+	    return "Sam";	
+	} //git add .
+	
+	public string Phone()
+	{
+		return "000";
+	} // git add / commit
+	
+	public string City()
+	{
+		return "Taipei"; 
+	} // git co -- .
+	
+} //git commit -m
+
+class City
+{
+	public string Name()
+	{
+		return "Taipei";
+	}
+} // git commit -m 
+
+//git log
+
+class Phone()
+{
+	public string Number()
+	{
+		return "0123456789";
+	}
+}
